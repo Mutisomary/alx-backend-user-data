a968897efea487fd84fd9b5ab4efee1e13c9ff0e
Mutisomary <marymutso2016@gmail.com>
2024-02-15 10:34:58 -0800

    session authentication

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,54 +1,42 @@
 #!/usr/bin/env python3
-"""Authentication"""
-
+"""
+API authentication module
+"""
 
 from flask import request
 from typing import List, TypeVar
-User = TypeVar('User')
+from os import getenv
 
 
 class Auth:
-    """Auth class"""
+    """ Authentication """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Authentication logic"""
-        # Return True if path is None
-        if path is None:
+        """ Checks if API routes require authentication """
+        if path is None or not excluded_paths:
             return True
-
-        # Return True if excluded_paths is None or empty
-        if excluded_paths is None or not excluded_paths:
-            return True
-
-        # Ensure path ends with a slash
-        if not path.endswith('/'):
-            path += '/'
-
-        # Check if the path is in excluded_paths
-        for excluded_path in excluded_paths:
-            # Ensure excluded_path ends with a slash
-            if not excluded_path.endswith('/'):
-                excluded_path += '/'
-
-            # Check if the path starts with the excluded_path (slash-tolerant)
-            if path.startswith(excluded_path):
+        for i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
                 return False
-
-        # If the path is not in excluded_paths, return True
+            elif i in {path, path + '/'}:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """authentication header"""
-        # Check if request is None
+        """ Checks if Authorization request header is present
+        & contains values """
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ placeholder """
+        return None
+
+    def session_cookie(self, request=None):
+        """ Returns cookie value from a request """
         if request is None:
             return None
 
-        # Check if the Authorization header is present in the request
-        if 'Authorization' not in request.headers:
-            return None
-
-        # Return the value of the Authorization header
-        return request.headers['Authorization']
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """current user"""
-        return None
+        return request.cookies.get(getenv('SESSION_NAME')
