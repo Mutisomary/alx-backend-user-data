04a1853883d14536c258557212d19b35414b7587
Mutisomary <marymutso2016@gmail.com>
2024-02-15 10:14:42 -0800

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Authentication"""
+
+
+from flask import request
+from typing import List, TypeVar
+User = TypeVar('User')
+
+
+class Auth:
+    """Auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Authentication logic"""
+        # Return True if path is None
+        if path is None:
+            return True
+
+        # Return True if excluded_paths is None or empty
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        # Ensure path ends with a slash
+        if not path.endswith('/'):
+            path += '/'
+
+        # Check if the path is in excluded_paths
+        for excluded_path in excluded_paths:
+            # Ensure excluded_path ends with a slash
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+
+            # Check if the path starts with the excluded_path (slash-tolerant)
+            if path.startswith(excluded_path):
+                return False
+
+        # If the path is not in excluded_paths, return True
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """authentication header"""
+        # Check if request is None
+        if request is None:
+            return None
+
+        # Check if the Authorization header is present in the request
+        if 'Authorization' not in request.headers:
+            return None
+
+        # Return the value of the Authorization header
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user"""
+        return None
